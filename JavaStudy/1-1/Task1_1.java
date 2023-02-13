456305861e8c2ac7c09b92b4a54f439df41a6cb7
ITONAOHITO <mp7vectork10pdw@gmail.com>
2023-02-13 19:14:50 +0900

    課題提出1-1

---
+++ JavaStudy/1-1/Task1_1.java
@@ -0,0 +1,16 @@
+public class Task1_1{
+
+    public static void main(String[] args) {
+        int hogeInt = 50;
+        String hogeString = "テスト";
+        // hogeDoubleという名前の変数を宣言。小数点の扱えるdouble型の値'0.2'で初期化。
+        double hogeDouble = 0.2;
+        hogeInt = 100;
+        hogeString = "合格";
+        System.out.println(hogeInt);
+        // 上記で宣言されている変数hogeStringの内容”合格”を表示させる
+        System.out.println(hogeString);
+        System.out.println(hogeDouble);
+
+    }
+}
