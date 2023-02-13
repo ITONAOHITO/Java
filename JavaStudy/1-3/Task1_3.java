205b11e1197b865e5b0ded055746626d118991e4
ITONAOHITO <mp7vectork10pdw@gmail.com>
2023-02-13 20:28:53 +0900

    課題提出1-3

---
+++ JavaStudy/1-3/Task1_3.java
@@ -0,0 +1,22 @@
+public class Task1_3 {
+
+    public static void main(String[] arg) {
+
+        int plus = 5 + 10;
+        int minus = 20 - 7;
+        // 10を2で乗算した値を代入する変数multiplyを作成。
+        int multiply = 10 * 2;
+        int remi = 20 % 6;
+        // String型の変数townとlineに、有楽町と線を代入。
+        String town = "有楽町";
+        String line = "線";
+        String train = "有楽町" + "線";
+
+        System.out.println(plus);
+        System.out.println(minus);
+        System.out.println(multiply);
+        System.out.println(remi);
+        System.out.println(train);
+
+    }
+}
