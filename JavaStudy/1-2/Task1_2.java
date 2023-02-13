5e68cdae002028c85f5c2f801101d2808beee2fb
ITONAOHITO <mp7vectork10pdw@gmail.com>
2023-02-13 20:08:08 +0900

    課題提出1-2

---
+++ JavaStudy/1-2/Task1_2.java
@@ -0,0 +1,15 @@
+public class Task1_2{
+
+    public static void main(String[] args) {
+        
+        String [] country = {"JAPAN", "AMERICA","KOREA"};
+        System.out.println(country.length);
+        String [] strArray = {"りんご", "もも", "ぶどう"};
+        System.out.println(strArray[1]);
+        // int型の10,20,30,40,50の5つの値を代入した配列intArrayを宣言。
+        int[] intArray = {10, 20, 30, 40, 50};
+        //intArray配列の二番目と五番目を値を表示させる。
+        System.out.println(intArray[1] + intArray[4]); 
+
+    }
+}
