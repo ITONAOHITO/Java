c0e7aac17db907547779b9a762ed74074fb38c55
ITONAOHITO <mp7vectork10pdw@gmail.com>
2023-02-16 15:12:03 +0900

    課題提出2-2

---
+++ JavaStudy/2-2/src/study/Main.java
@@ -0,0 +1,11 @@
+package study;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		User user = new User("hoge", 123, "abc" + 123);
+		user.printAccountInfo();
+	}
+
+}
